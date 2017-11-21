82e35a79d8109964083afe5cb90ea05ff38d15f1
Xiaoyan Qu <xiaoyanqu@hotmail.com>
2017-11-21 14:54:24 -0800

    Update so that data with time stamps, output data ready to use out of request.log

--- Assignment6/src/main.cpp
+++ Assignment6/src/main.cpp
@@ -5,11 +5,21 @@
 #include "SPI.h"
 #include "SparkFunLIS3DH.h"
 #include "MAX30105.h"
+// #include "heartRate.h"
+
 
 
 String deviceName = "CS244";
 LIS3DH acc_sensor(SPI_MODE, 4);
-MAX30105 hr_sensor;
+MAX30105 particleSensor;
+
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+
+float beatsPerMinute;
+int beatAvg;
 
 // WiFi settings
 // const char *ssid = "NETGEAR89";
@@ -38,13 +48,15 @@ void setup()
     Serial.println("Start moving.\n");
     // Serial.println("Start sensing.\n");
 
-    if (!hr_sensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed: begin(Wire, I2C_SPEED_FAST)
+    if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed: begin(Wire, I2C_SPEED_FAST)
     {
         Serial.println("MAX30105 was not found. Please check wiring/power. ");
         while (1);
     }
 
-    hr_sensor.setup();
+    particleSensor.setup();
+    // particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+    // particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
 
     // Initialize sensor
     acc_sensor.begin();
@@ -52,17 +64,8 @@ void setup()
 
 void loop()
 {
-    // Serial.print(" R[");
-    // Serial.print(hr_sensor.getRed());
-    // Serial.print("] IR[");
-    // Serial.print(hr_sensor.getIR());
-    // Serial.print("] G[");
-    // Serial.print(hr_sensor.getGreen());
-    // Serial.print("]");
-    // delay(1000);
-    // Serial.println();
-    String ir = String(hr_sensor.getIR());
-    String red = String(hr_sensor.getRed());
+    String ir = String(particleSensor.getIR());
+    String red = String(particleSensor.getRed());
 
     Serial.print("IR = " + ir);
     Serial.print(" R = " + red);
@@ -74,18 +77,51 @@ void loop()
     Serial.print(" X = " + x);
     Serial.print(" Y = " + y);
     Serial.println(" Z = " + z);
+
+    // if (checkForBeat(irValue) == true)
+    // {
+    //     //We sensed a beat!
+    //     long delta = millis() - lastBeat;
+    //     lastBeat = millis();
+
+    //     beatsPerMinute = 60 / (delta / 1000.0);
+
+    //     if (beatsPerMinute < 255 && beatsPerMinute > 20)
+    //     {
+    //         rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+    //         rateSpot %= RATE_SIZE; //Wrap variable
+
+    //         //Take average of readings
+    //         beatAvg = 0;
+    //         for (byte x = 0 ; x < RATE_SIZE ; x++)
+    //         beatAvg += rates[x];
+    //         beatAvg /= RATE_SIZE;
+    //     }
+    // }
+
+    // Serial.print(", BPM=");
+    // Serial.print(beatsPerMinute);
+    // Serial.print(", Avg BPM=");
+    // Serial.print(beatAvg);
+
+    // if (irValue < 50000)
+    // Serial.print(" No finger?");
+
+    // Serial.println();
     
-    String reading = "ir=" + ir + "&red=" + red + "&x=" + x + "&y=" + y + "&z=" + z;
+    String reading = "&ir=" + ir + "&red=" + red + "&x=" + x + "&y=" + y + "&z=" + z;
     
     if (WiFi.status() == WL_CONNECTED) {
         WiFiClient client;
         const int httpPort = 80;
                 
-        if (!client.connect("169.234.64.169", httpPort)) {
+        if (!client.connect("169.234.51.114", httpPort)) {
             Serial.println("Server connection failed");
             return;
         }
-
+        String curTime = String(millis());
+        Serial.println("Time : " + curTime);
+        reading = "time=" + curTime + reading;
         String url = "GET /data.php?" + reading + " HTTP/1.0";
         client.println(url);
         client.println();
