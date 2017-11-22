f4fb076ce1b538770c16a12c6d27e592d3ea904b
Xiaoyan Qu <xiaoyanqu@hotmail.com>
2017-11-21 22:22:43 -0800

    Code for assignment 6.

--- Assignment6/src/main.cpp
+++ Assignment6/src/main.cpp
@@ -22,12 +22,12 @@ float beatsPerMinute;
 int beatAvg;
 
 // WiFi settings
-// const char *ssid = "NETGEAR89";
-const char *ssid = "UCInet Mobile Access";
+const char *ssid = "NETGEAR89";
+// const char *ssid = "UCInet Mobile Access";
 
 void connectToWiFi()
 {
-    WiFi.begin(ssid);
+    WiFi.begin(ssid, "yyyqqq2017");
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
@@ -40,8 +40,6 @@ void connectToWiFi()
 
 void setup()
 {
-    // Wire.pins(0, 4);
-    // Wire.begin();
     Serial.begin(9600);
     connectToWiFi();
     delay(1000); //relax...
@@ -55,8 +53,6 @@ void setup()
     }
 
     particleSensor.setup();
-    // particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
-    // particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
 
     // Initialize sensor
     acc_sensor.begin();
@@ -77,37 +73,6 @@ void loop()
     Serial.print(" X = " + x);
     Serial.print(" Y = " + y);
     Serial.println(" Z = " + z);
-
-    // if (checkForBeat(irValue) == true)
-    // {
-    //     //We sensed a beat!
-    //     long delta = millis() - lastBeat;
-    //     lastBeat = millis();
-
-    //     beatsPerMinute = 60 / (delta / 1000.0);
-
-    //     if (beatsPerMinute < 255 && beatsPerMinute > 20)
-    //     {
-    //         rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-    //         rateSpot %= RATE_SIZE; //Wrap variable
-
-    //         //Take average of readings
-    //         beatAvg = 0;
-    //         for (byte x = 0 ; x < RATE_SIZE ; x++)
-    //         beatAvg += rates[x];
-    //         beatAvg /= RATE_SIZE;
-    //     }
-    // }
-
-    // Serial.print(", BPM=");
-    // Serial.print(beatsPerMinute);
-    // Serial.print(", Avg BPM=");
-    // Serial.print(beatAvg);
-
-    // if (irValue < 50000)
-    // Serial.print(" No finger?");
-
-    // Serial.println();
     
     String reading = "&ir=" + ir + "&red=" + red + "&x=" + x + "&y=" + y + "&z=" + z;
     
@@ -115,7 +80,7 @@ void loop()
         WiFiClient client;
         const int httpPort = 80;
                 
-        if (!client.connect("169.234.51.114", httpPort)) {
+        if (!client.connect("169.234.219.221", httpPort)) {
             Serial.println("Server connection failed");
             return;
         }
